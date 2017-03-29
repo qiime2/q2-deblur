134b3adcf95ccbc7272dd0c5517e551056965384
Matthew Dillon <matthewrdillon@gmail.com>
2017-03-29 16:48:33 -0700

    BUG: Missing comma (comma coma) (#5)

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         ["q2-deblur=q2_deblur.plugin_setup:plugin"]
     },
     package_data={
-        "q2_deblur": ["assets/index.html"]
+        "q2_deblur": ["assets/index.html"],
         'q2_deblur.tests': ['data/*',
                             'data/expected/*',
                             'data/expected/util/*',
