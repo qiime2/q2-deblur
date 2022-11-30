fefaec5e9fafbf7ae427cce0b13ded4afde8b6eb
Liz Gehret <elizabeth.gehret@gmail.com>
2022-11-30 15:57:06 -0700

    squash: adding commands into test runner script

--- ci/recipe/run_test.sh
+++ ci/recipe/run_test.sh
@@ -3,3 +3,7 @@
 git clone https://github.com/biocore/deblur deblur-upstream
 nosetests --with-doctest deblur-upstream
 rm -rf deblur-upstream
+
+git clone https://github.com/qiime2/q2-deblur q2_deblur
+py.test --pyargs q2_deblur
+rm -rf q2_deblur
