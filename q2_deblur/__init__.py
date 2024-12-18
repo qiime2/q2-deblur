f669ae9f84f2da94a09ef6186af44ff2bff9f746
q2d2 <q2d2.noreply@gmail.com>
2024-12-18 22:04:19 +0000

    Transition from setup.py & friends to pyproject.toml

--- q2_deblur/__init__.py
+++ q2_deblur/__init__.py
@@ -9,10 +9,11 @@
 from ._denoise import denoise_16S, denoise_other
 from ._viz_stats import visualize_stats
 
-from ._version import get_versions
 
 
-__version__ = get_versions()['version']
-del get_versions
+try:
+    from ._version import __version__
+except ModuleNotFoundError:
+    __version__ = '0.0.0+notfound'
 
 __all__ = ['denoise_16S', 'denoise_other', 'visualize_stats']
